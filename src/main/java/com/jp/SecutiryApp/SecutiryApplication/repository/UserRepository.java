ae877e9bb1e67ef3a75544f4573db8e543c4635a
Jitu-77 <jyotirmoypal077@gmail.com>
2026-06-23 15:02:12 +0530

    User entity,service,repository made userDetails are implemented

---
+++ src/main/java/com/jp/SecutiryApp/SecutiryApplication/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.jp.SecutiryApp.SecutiryApplication.repository;
+
+import com.jp.SecutiryApp.SecutiryApplication.entity.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<UserEntity,Long> {
+    Optional<UserEntity> findByEmail(String email);
+}
