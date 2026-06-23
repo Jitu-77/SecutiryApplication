a6b96e2ab6381f8a35734f49a6af8f711453d41a
Jitu-77 <jyotirmoypal077@gmail.com>
2026-06-23 19:46:21 +0530

    JWT basic token set up is done , 5.5

--- src/main/java/com/jp/SecutiryApp/SecutiryApplication/entity/UserEntity.java
+++ src/main/java/com/jp/SecutiryApp/SecutiryApplication/entity/UserEntity.java
@@ -1,6 +1,10 @@
 package com.jp.SecutiryApp.SecutiryApplication.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.jspecify.annotations.Nullable;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -8,7 +12,11 @@ import org.springframework.security.core.userdetails.UserDetails;
 import java.util.Collection;
 import java.util.List;
 
+@Getter
+@Setter
 @Entity
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserEntity implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -16,6 +24,7 @@ public class UserEntity implements UserDetails {
 
     @Column(unique = true)
     private String email;
+
     private String password;
 
     @Override
@@ -24,7 +33,7 @@ public class UserEntity implements UserDetails {
     }
 
     @Override
-    public @Nullable String getPassword() {
+    public String getPassword() {
        return  this.password;
     }
 
--- src/test/java/com/jp/SecutiryApp/SecutiryApplication/SecutiryApplicationTests.java
+++ src/test/java/com/jp/SecutiryApp/SecutiryApplication/SecutiryApplicationTests.java
@@ -1,13 +1,27 @@
 package com.jp.SecutiryApp.SecutiryApplication;
 
+import com.jp.SecutiryApp.SecutiryApplication.entity.UserEntity;
+import com.jp.SecutiryApp.SecutiryApplication.service.JwtService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SecutiryApplicationTests {
-
+	@Autowired
+	private JwtService jwtService;
 	@Test
 	void contextLoads() {
+		UserEntity user = new UserEntity(4L, "jp@gmail.com", "1234");
+
+		String token = jwtService.generateToken(user);
+
+		System.out.println(token);
+
+//		Long id = jwtService.getUserIdFromToken(token);
+		Long id = jwtService.getUserIdFromToken("eyJhbGciOiJIUzM4NCJ9.eyJzdWIiOiI0IiwiZW1haWwiOiJqcEBnbWFpbC5jb20iLCJyb2xlcyI6WyJVU0VSIiwiQURNSU4iXSwiaWF0IjoxNzgyMjIzODMxLCJleHAiOjE3ODIyMjM4OTF9.8Zk9GAQCJPZQFlMRa9_CcMGNnzlX0Fflp4UWhIhE__rW6vZTW5IER-SUrqd4zxh5");
+
+		System.out.println(id);
 	}
 
 }
